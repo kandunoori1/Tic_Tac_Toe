cc051b0428985558508ce5c3a61ee30d20e0b468
krishna <kris@gmail.com>
2023-01-03 20:03:47 -0500

    test

---
+++ main.java
@@ -0,0 +1,34 @@
+public class TicTacToe {
+
+public static void main(String [] args){
+    
+   /**  Notes:
+        _ | _ | _
+        _ | _ | _
+          |   |
+         Helpful indices
+         [0][0] , [0][2] , [0][4]
+         [1][0] , [1][2] , [1][4]
+         [2][0] , [2][2] , [2][4]
+         
+         **/
+
+    char[][] gameBoard = {
+      {'_','|','_','|','_'},
+      {'_','|','_','|','_'},
+      {' ','|',' ','|',' '}
+    };
+    
+    printBoard(gameBoard);
+   
+}
+  public static void printBoard(char [][] gameBoard){
+
+    for(char[] row : gameBoard){
+        for( char c : row){
+            System.out.print(c);
+        }
+        System.out.println();
+    }
+  }
+}
